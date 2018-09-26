9cfebc731a9f4e10de42e16d38482a5917edd1fd
Christopher Millington <30037043@westernsydney.edu.au>
2018-09-26 15:55:30 +1000

    Final files for 26/09

--- prac/py/application.py
+++ prac/py/application.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session  
 
+#global variables
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "super secret key"
+Session(app)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-        return render_template("index.html")
+    if session.get("notes") is None:                #if the note dictionary is not initialised,
+        session["notes"] = []                       #then initialise a dictionary that is unique to your session
+    if request.method == "POST":                    #if i tried to add a note.. POST method            
+        note = request.form.get("note")             #grab the note from the form that was sent to this function
+        session["notes"].append(note)               #add a new thing to the list of notes
 
-@app.route("/hello", methods=["POST"])
+    return render_template("notes.html", notes=session["notes"])
+    
+
+
+@app.route("/hello", methods=["GET", "POST"])
 def hello():
-        name = request.form.get("name")
-        return render_template("hello.html", name=name)
+        if request.method == "GET":
+                return "Please submit the form instead."
+        else:
+            name = request.form.get("name")
+            return render_template("hello.html", name=name)
+
