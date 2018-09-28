03ece2aa1bda5fb6c30450ec6d5a7fe83c4c8538
B4LLL <c.millington@westernsydney.edu.au>
2018-09-28 12:24:06 +1000

    28/09

---
+++ prac/SQL/insert_passengers.sql
@@ -0,0 +1,5 @@
+INSERT passengers (name, flight_id) VALUES ('Alice', 1);
+INSERT passengers (name, flight_id) VALUES ('Bob', 1);
+INSERT passengers (name, flight_id) VALUES ('Jane', 2);
+INSERT passengers (name, flight_id) VALUES ('Muhammed', 4);
+INSERT passengers (name, flight_id) VALUES ('Sarah', 5);
